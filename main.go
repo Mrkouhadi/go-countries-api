aaecb2e27b17f67993e05b09a74df552177c0f39
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2023-08-10 03:21:45 +0800

    perf: removed unused code

--- main.go
+++ main.go
@@ -1,25 +1,9 @@
 package main
 
 import (
-	"encoding/json"
-	"os"
-	"strings"
+	"fmt"
 )
 
 func main() {
-	EditCountriesFlags()
-}
-
-func EditCountriesFlags() {
-	file, _ := os.ReadFile("./data/countries-with-cities.json")
-	countries := []NewCountryWithCities{}
-	_ = json.Unmarshal(file, &countries)
-
-	for i := 0; i < len(countries); i++ {
-		cc := strings.ToLower(countries[i].Code)
-		countries[i].Flag = "./flags-svg/" + cc + ".svg"
-	}
-	// write a file of json format
-	jsonfile, _ := json.MarshalIndent(countries, "", "    ")
-	_ = os.WriteFile("./data/countries-with-svg-flags-cities.json", jsonfile, 0644)
+	fmt.Println("CONNECTED...")
 }
